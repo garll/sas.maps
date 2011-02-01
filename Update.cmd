e8f07414f514184654581f645717feba19f031ae
Tol <none@none>
2011-02-01 18:58:05 +0300

    + Update.cmd

---
+++ Update.cmd
@@ -0,0 +1,28 @@
+@echo off
+hg incoming "https://bitbucket.org/vdemidov/sas.maps/"
+::echo %ERRORLEVEL%
+IF ERRORLEVEL 9009 goto NoHg
+IF ERRORLEVEL 2 goto err
+IF ERRORLEVEL 1 goto noupdates
+IF ERRORLEVEL 0 goto ok
+
+goto err
+
+:ok
+        echo Забираем изменения из репозитория
+        hg pull "https://bitbucket.org/vdemidov/sas.maps/" -u -f
+        IF ERRORLEVEL 1 goto err
+        IF NOT ERRORLEVEL 0 goto err
+rem        call BuildZMmp.cmd
+        goto end
+:noupdates
+        echo Нет новых изменений
+        goto end
+:err
+        echo Ошибка связи с сервером
+        goto end
+:NoHg
+        echo Не установлен Mercurial
+        goto end
+:end
+pause
