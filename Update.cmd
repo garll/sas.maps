8d4392c0b7a498aecbc6e5ba0d8e993d148f201c
Viktor Demydov <vdemidov@gmail.com>
2011-08-01 10:24:52 +0300

    Сделал батник, который делает клон репозитория, если его еще нет

--- Update.cmd
+++ Update.cmd
@@ -5,6 +5,7 @@ IF ERRORLEVEL 9009 goto NoHg
 IF ERRORLEVEL 2 goto err
 IF ERRORLEVEL 1 goto noupdates
 IF ERRORLEVEL 0 goto ok
+IF ERRORLEVEL -1 goto CloneRepo
 
 goto err
 
@@ -13,13 +14,32 @@ goto err
         hg pull "https://bitbucket.org/vdemidov/sas.maps/" -u -f
         IF ERRORLEVEL 1 goto err
         IF NOT ERRORLEVEL 0 goto err
-rem        call BuildZMmp.cmd
+        goto end
+:CloneRepo
+	rd /s /q sas.maps
+	echo Делаем клон репозитория с сервера
+	hg clone -U "https://bitbucket.org/vdemidov/sas.maps/" sas.maps
+        IF NOT ERRORLEVEL 0 goto err
+	echo Копируем папку с репозиторием из подпапки в текущую папку
+	move /Y sas.maps\.hg .\.hg
+        IF NOT ERRORLEVEL 0 goto errMoveHg
+	echo Удаляем временно созданную подпапку
+	rd /s /q sas.maps
+        IF NOT ERRORLEVEL 0 goto errRemoveTemp
+	echo Обновляем файлы до последней версии
+	hg update -c
         goto end
 :noupdates
         echo Нет новых изменений
         goto end
 :err
         echo Ошибка связи с сервером
+        goto end
+:errMoveHg
+        echo Ошибка перемещения папки .hg 
+        goto end
+:errRemoveTemp
+        echo Ошибка удаления временной папки sas.maps 
         goto end
 :NoHg
         echo Не установлен Mercurial
